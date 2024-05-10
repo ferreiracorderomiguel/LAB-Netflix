88d30b7ed298b325bab33f90e0839a71e0b63f34
Miguel Ferreira Cordero <72079312+ferreiracorderomiguel@users.noreply.github.com>
2024-05-10 16:59:20 +0200

    Update CatalogoNetflix.java

--- src/fp/netflix/CatalogoNetflix.java
+++ src/fp/netflix/CatalogoNetflix.java
@@ -1,5 +1,41 @@
 package fp.netflix;
 
-public class CatalogoNetflix {
+import java.util.Objects;
+import java.util.SortedSet;
 
+public class CatalogoNetflix {
+	// Atributo
+	private SortedSet<ProduccionNetflix> producciones;
+
+	// Constructor
+	public CatalogoNetflix(SortedSet<ProduccionNetflix> producciones) {
+		this.producciones = producciones;
+	}
+	
+	// Métodos
+	public SortedSet<ProduccionNetflix> getProducciones() {
+		return producciones;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(producciones);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CatalogoNetflix other = (CatalogoNetflix) obj;
+		return Objects.equals(producciones, other.producciones);
+	}
+
+	@Override
+	public String toString() {
+		return "CatalogoNetflix [producciones=" + producciones + "]";
+	}
 }
